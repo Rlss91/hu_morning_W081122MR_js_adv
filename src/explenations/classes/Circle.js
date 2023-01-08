25aa8699c91076b2b3ca3a13f4811fb107330c1c
Rlss91 <rlss91@gmail.com>
2023-01-08 13:57:15 +0200

    classes

--- src/explenations/classes/Circle.js
+++ src/explenations/classes/Circle.js
@@ -1,20 +1,40 @@
 class Circle {
   x;
   y;
-  r;
-  a;
-  p;
+  #r;
+  #a;
+  #p;
   constructor(x, y, r) {
     this.x = x;
     this.y = y;
-    this.r = r;
+    this.#r = r;
     this.calcA();
     this.calcP();
   }
   calcA() {
-    this.a = this.r * this.r * Math.PI;
+    this.#a = this.#r * this.#r * Math.PI;
   }
   calcP() {
-    this.p = 2 * this.r * Math.PI;
+    this.#p = 2 * this.#r * Math.PI;
+  }
+  get r() {
+    return this.#r;
+  }
+  get a() {
+    return this.#a;
+  }
+  get p() {
+    return this.#p;
+  }
+  set r(newR) {
+    if (newR > 0) {
+      this.#r = newR;
+      this.calcA();
+      this.calcP();
+    }
   }
 }
+
+let c = new Circle(0, 0, 5);
+c.r = 10;
+console.log(`r = ${c.r}, a = ${c.a}, p = ${c.p}`);
