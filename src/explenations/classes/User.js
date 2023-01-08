01718ef88fd71f2a52d932e58ef7eb085377cad1
Rlss91 <rlss91@gmail.com>
2023-01-08 14:02:49 +0200

    class user

---
+++ src/explenations/classes/User.js
@@ -0,0 +1,15 @@
+class User {
+  name;
+  age;
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+// let user = {
+//   name: "kenny",
+//   age: 8,
+// };
+
+let user = new User("kenny", 8);
