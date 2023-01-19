39c97130fad39071c92b8ccc7b2283b1445edd56
Rlss91 <rlss91@gmail.com>
2023-01-19 10:26:28 +0200

    hw

---
+++ src/explenations/promise/hw.js
@@ -0,0 +1,39 @@
+const findMaxIndexPromise = (arr) => {
+  return new Promise((resolve, reject) => {
+    if (!arr) {
+      // findMaxIndexPromise()
+      reject("the array undefined");
+    }
+    if (!arr.length) {
+      // findMaxIndexPromise([])
+      reject("the array empty");
+    }
+    if (!Array.isArray(arr)) {
+      // findMaxIndexPromise(5)
+      reject("not arr");
+    }
+    let maxInx = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] !== "number") {
+        reject("need numbers arr");
+        break;
+      }
+      if (arr[maxInx] < arr[i]) {
+        maxInx = i;
+      }
+    }
+    resolve(maxInx);
+  });
+};
+
+const testOurPromise = async () => {
+  try {
+    let arrOfNumber = [1, 2, 3, 44, 5];
+    //[-1, -2, -3]
+    let maxInx = await findMaxIndexPromise(arrOfNumber);
+    console.log("max index", maxInx);
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+testOurPromise();
