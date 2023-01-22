fb74a40587a86773a69d062945f05c9d4322bf84
Rlss91 <rlss91@gmail.com>
2023-01-22 15:59:36 +0200

    carusel not finished

--- src/components/PropertiesCarousel.js
+++ src/components/PropertiesCarousel.js
@@ -1,15 +1,42 @@
 let propertiesArr;
 let carouselDiv;
+let showIdx = 0; //index(array) of the image that we display now
 //this function will transfer data from homepage to this page
 const initialPropertiesCarousel = (propertiesArrFromHomePage) => {
   propertiesArr = propertiesArrFromHomePage;
   carouselDiv = document.getElementById("home-page-properties-carousel");
   createCarousel();
+  document
+    .getElementById("back-carusel-btn")
+    .addEventListener("click", () => {});
+  document.getElementById("next-carusel-btn").addEventListener("click", () => {
+    let nextIdx = showIdx + 1;
+    //showIdx = index of image to hide
+    //nextIdx = index of image to display
+    let imgToHide = document.querySelector(
+      `.img-container > img:nth-child(${showIdx + 1})`
+    );
+    imgToHide.classList.add("fade-out");
+    imgToHide.addEventListener("animationend", () => {
+      imgToHide.classList.add("opacity-0");
+      imgToHide.classList.remove("fade-out");
+    });
+    let imgToShow = document.querySelector(
+      `.img-container > img:nth-child(${nextIdx + 1})`
+    );
+    imgToShow.classList.remove("opacity-0");
+    imgToShow.classList.add("fade-in");
+    imgToShow.addEventListener("animationend", () => {
+      imgToShow.classList.remove("fade-in");
+    });
+    showIdx++;
+  });
 };
 
 const createItem = (name, img) => {
+  //opacity-0 hide image
   return `
-      <img src="${img}" alt="${name}" />
+      <img src="${img}" alt="${name}" class="opacity-0" />
   `;
 };
 
@@ -17,6 +44,10 @@ const createCarousel = () => {
   for (let property of propertiesArr) {
     carouselDiv.innerHTML += createItem(property.name, property.imgUrl);
   }
+  //show the first img
+  document
+    .querySelector(".img-container > img:nth-child(1)")
+    .classList.remove("opacity-0");
 };
 
 export default initialPropertiesCarousel;
