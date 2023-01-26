9199080ad46cbdfae45ff8bfc3241b5dc7f47503
Rlss91 <rlss91@gmail.com>
2023-01-26 13:00:59 +0200

    home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,16 @@ import initialPropertiesList from "../components/PropertiesList.js";
 import initialPropertiesCarousel from "../components/PropertiesCarousel.js";
 
 let propertiesArr;
+let displayNow; // display that we can see now
+
+/* btns */
+let homeDisplayList;
+let homeDisplayGallery;
+let homeDisplayCousel;
+/* displays */
+let propertiesGallery;
+let propertiesList;
+let propertiesCarusel;
 
 window.addEventListener("load", () => {
   propertiesArr = localStorage.getItem("props");
@@ -15,4 +25,38 @@ window.addEventListener("load", () => {
   initialPropertiesGallery(propertiesArr);
   initialPropertiesList(propertiesArr);
   initialPropertiesCarousel(propertiesArr);
+  /* btns */
+  homeDisplayList = document.getElementById("homeDisplayList");
+  homeDisplayGallery = document.getElementById("homeDisplayGallery");
+  homeDisplayCousel = document.getElementById("homeDisplayCousel");
+  /* divs */
+  propertiesGallery = document.getElementById("propertiesGallery");
+  propertiesList = document.getElementById("propertiesList");
+  propertiesCarusel = document.getElementById("propertiesCarusel");
+  displayNow = propertiesGallery; // choose who we want to display
+  displayToDisplay(displayNow);
+  initializeBtns();
 });
+
+const initializeBtns = () => {
+  homeDisplayList.addEventListener("click", () => {
+    displayToDisplay(propertiesList);
+  });
+  homeDisplayGallery.addEventListener("click", () => {
+    displayToDisplay(propertiesGallery);
+  });
+  homeDisplayCousel.addEventListener("click", () => {
+    displayToDisplay(propertiesCarusel);
+  });
+};
+
+const displayToDisplay = (toDisplay) => {
+  // hide what we currently showing
+  displayNow.classList.remove("d-block");
+  displayNow.classList.add("d-none");
+  // show what we want to display now
+  toDisplay.classList.remove("d-none");
+  toDisplay.classList.add("d-block");
+  //this is what we displaying now
+  displayNow = toDisplay;
+};
