198c57980d6628e85df99e4c999475805a22bf1a
Rlss91 <rlss91@gmail.com>
2023-01-26 11:26:59 +0200

    carusel next

--- src/components/PropertiesCarousel.js
+++ src/components/PropertiesCarousel.js
@@ -1,6 +1,7 @@
 let propertiesArr;
 let carouselDiv;
 let showIdx = 0; //index(array) of the image that we display now
+let animationStarted = 0;
 //this function will transfer data from homepage to this page
 const initialPropertiesCarousel = (propertiesArrFromHomePage) => {
   propertiesArr = propertiesArrFromHomePage;
@@ -10,26 +11,47 @@ const initialPropertiesCarousel = (propertiesArrFromHomePage) => {
     .getElementById("back-carusel-btn")
     .addEventListener("click", () => {});
   document.getElementById("next-carusel-btn").addEventListener("click", () => {
+    if (animationStarted !== 0) {
+      return;
+    }
+    animationStarted = 2;
     let nextIdx = showIdx + 1;
     //showIdx = index of image to hide
     //nextIdx = index of image to display
+    if (nextIdx >= propertiesArr.length) {
+      nextIdx = 0;
+    }
     let imgToHide = document.querySelector(
       `.img-container > img:nth-child(${showIdx + 1})`
     );
     imgToHide.classList.add("fade-out");
-    imgToHide.addEventListener("animationend", () => {
+    const hideImgAnim = () => {
+      imgToHide.removeEventListener("animationend", hideImgAnim); //remove event after executed
       imgToHide.classList.add("opacity-0");
       imgToHide.classList.remove("fade-out");
-    });
+      console.log("opacity-0 added, showIdx", showIdx);
+      animationStarted--;
+    };
+    imgToHide.addEventListener("animationend", hideImgAnim);
     let imgToShow = document.querySelector(
       `.img-container > img:nth-child(${nextIdx + 1})`
     );
     imgToShow.classList.remove("opacity-0");
     imgToShow.classList.add("fade-in");
-    imgToShow.addEventListener("animationend", () => {
-      imgToShow.classList.remove("fade-in");
-    });
-    showIdx++;
+    imgToShow.addEventListener(
+      "animationend",
+      () => {
+        // imgToShow.classList.remove("opacity-0");
+        imgToShow.classList.remove("fade-in");
+        animationStarted--;
+      },
+      { once: true }
+    );
+    // showIdx++;
+    // if (showIdx >= propertiesArr.length) {
+    //   showIdx = 0;
+    // }
+    showIdx = nextIdx;
   });
 };
 
